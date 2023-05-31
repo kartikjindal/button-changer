928870796475a68ee4ee494dfbc11423d9540feb
kartikjindal <72238165+kartikjindal@users.noreply.github.com>
2023-05-31 12:45:15 +0530

    Initial commit

---
+++ javajava.js
@@ -0,0 +1,19 @@
+
+console.log('kartik');
+let btn = document.getElementsByClassName('btn');
+const func = () => {
+    let a = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let c = Math.floor(Math.random() * 256);
+    return `rgb(${a},${b},${c})`;
+}
+console.log(btn[0]);
+btn[0].addEventListener('click',() => {
+    const str = func();
+    console.log(str);
+    document.body.style.backgroundColor= str;
+    console.log(document.body.children[0].children[0])
+});
+
+// btn[0].innerHTML=`<div> hi </div>`
+// btn.addEventListener('click', ()=>{ console.log('hello')} )
